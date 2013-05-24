a0c71f9735213bc44d38fd7c4d603a9fa5f16828
Wing Lung Ngai <w.l.ngai@student.tudelft.nl>
2013-05-24 23:48:53 +0200

    Add all queries

--- Ch5Project1/src/MovieRetriever.java
+++ Ch5Project1/src/MovieRetriever.java
@@ -18,8 +18,76 @@ public class MovieRetriever {
 		XMLDatabaseConnector xConnector = new XMLDatabaseConnector();
 
 		MovieRetriever mRetriever = new MovieRetriever(xConnector);
-		ArrayList<String> movies = mRetriever.retrieveByYear(1999, 2005);
+		ArrayList<String> movies;
+		movies = mRetriever.retrieveByTitle("Heat");
 		System.out.println(movies.toString());
+		
+		movies = mRetriever.retrieveByGenre("Crime");
+		System.out.println(movies.toString());
+		
+		movies = mRetriever.retrieveByActorName("Scarlett J");
+		System.out.println(movies.toString());
+		
+		movies = mRetriever.retrieveByDirectorName("Clint");
+		System.out.println(movies.toString());
+		
+		movies = mRetriever.retrieveByKeyword("love");
+		System.out.println(movies.toString());
+		
+		
+	}
+	
+	/*
+	 * Retrieve by title.
+	 */
+	public ArrayList<String> retrieveByTitle(String title)
+	{
+		String collectionPath = "/db/movies";
+		
+		String xQuery = XQueryFileReader.Read("queries/retrieveByTitle.txt");
+		xQuery = xQuery.replace("#title", title);
+		
+		return xConnector.read(collectionPath, xQuery);
+	}
+	
+	/*
+	 * Retrieve by genre.
+	 */
+	public ArrayList<String> retrieveByGenre(String genre)
+	{
+		String collectionPath = "/db/movies";
+		
+		String xQuery = XQueryFileReader.Read("queries/retrieveByGenre.txt");
+		xQuery = xQuery.replace("#genre", genre);
+		
+		return xConnector.read(collectionPath, xQuery);
+	}
+
+	
+	/*
+	 * Retrieve by director name.
+	 */
+	public ArrayList<String> retrieveByDirectorName(String directorName)
+	{
+		String collectionPath = "/db/movies";
+		
+		String xQuery = XQueryFileReader.Read("queries/retrieveByDirectorName.txt");
+		xQuery = xQuery.replace("#directorName", directorName);
+		
+		return xConnector.read(collectionPath, xQuery);
+	}
+
+	/*
+	 * Retrieve by actor name.
+	 */
+	public ArrayList<String> retrieveByActorName(String actorName)
+	{
+		String collectionPath = "/db/movies";
+		
+		String xQuery = XQueryFileReader.Read("queries/retrieveByActorName.txt");
+		xQuery = xQuery.replace("#actorName", actorName);
+		
+		return xConnector.read(collectionPath, xQuery);
 	}
 	
 	/*
@@ -35,6 +103,20 @@ public class MovieRetriever {
 		
 		return xConnector.read(collectionPath, xQuery);
 	}
+	
+	/*
+	 * Retrieve by time interval in years.
+	 */
+	public ArrayList<String> retrieveByKeyword(String keyword)
+	{
+		String collectionPath = "/db/movies";
+		
+		String xQuery = XQueryFileReader.Read("queries/retrieveByKeyword.txt");
+		xQuery = xQuery.replace("#keyword", keyword);
+		
+		return xConnector.read(collectionPath, xQuery);
+	}
+	
 	
 //	public ArrayList<Integer> retrieveByYear(int earliest, int latest)
 //	{
