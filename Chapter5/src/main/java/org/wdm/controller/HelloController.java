a20af22b242aaf5b4d76f4c2b3c7b5d8ecbaca99
Szabo Marian <szabomarian@yahoo.com>
2013-05-25 17:27:49 +0200

    Get and Post examples

--- Chapter5/src/main/java/org/wdm/controller/HelloController.java
+++ Chapter5/src/main/java/org/wdm/controller/HelloController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.wdm.project1.UserRequest;
  
@@ -12,13 +14,23 @@ import org.wdm.project1.UserRequest;
 public class HelloController {
     protected final Logger logger = Logger.getLogger(getClass());
  
-    @RequestMapping("/startMapping")
-    public @ResponseBody List<String> handleRequest() {
+    @RequestMapping(value="/asyncCall", method=RequestMethod.GET)
+    public @ResponseBody List<String> handleGetRequest() {
     	
     	UserRequest uRequest = new UserRequest();
 		uRequest.setActorName("Scarlett");
 		uRequest.setTimeInterval(2002, 2006);
 		uRequest.setTitle("Translation");
+        
+        return uRequest.retrieveMovie();
+    }
+    
+    @RequestMapping(value="/asyncCall", method=RequestMethod.POST )
+    public @ResponseBody List<String> handlePostRequest(
+    		@RequestParam(value="title") String movieTitle, @RequestParam(value="genre") String genre) {
+    	UserRequest uRequest = new UserRequest();
+		uRequest.setTitle(movieTitle);
+		uRequest.setGenre(genre);
         
         return uRequest.retrieveMovie();
     }
--- Chapter5/src/main/webapp/index.jsp
+++ Chapter5/src/main/webapp/index.jsp
@@ -1,12 +1,36 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
 <html>
-	<head>
-		<title>DataMapper</title>
-	</head>
-	<body>
-		<h1>Hello world</h1>
-		<form method="get" action="startMapping">
-			<input type="submit"></input>
-		</form>
-	</body>
+<head>
+<title>Web Data Management - Chapter 5</title>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
+<script>
+			$(document).ready(function(){
+			  $("#getButton").click(function(){
+			    $.get("asyncCall",function(data){
+			      alert(data);
+			    });
+			  });
+			  
+			  $("#postButton").click(function(){
+				    $.post("asyncCall",	
+				    {
+				    	title: 'Lost in Translation',
+				    	genre: 'Drama'
+				    },		
+				    function(data){
+				      alert(data);
+				    });
+				  });
+			});
+
+</script>
+</head>
+<body>
+	<h1>Hello world</h1>
+	<button id="getButton">Send an HTTP GET request to a page and get the result back</button>
+	<br/>
+	<button id="postButton">Send an HTTP POST request to a page and get the result back</button>
+</body>
 </html>
